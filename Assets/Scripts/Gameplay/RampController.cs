6ba328410e2ba60ebf43423bf3a0fa5df1f4fc2a
onurtuncc <monurtunc.53@gmail.com>
2021-10-14 18:00:53 +0300

    two points per click

--- Assets/Scripts/Gameplay/RampController.cs
+++ Assets/Scripts/Gameplay/RampController.cs
@@ -15,6 +15,7 @@ public class RampController : MonoBehaviour
     private bool isInRampPhase=false;
     [HideInInspector] public float fillAmount=0;
     private float throwPoint = 0;
+    private float pointPerClick = 2f;
     Sequence rampSequence;
     Transform playerTransform;
 
@@ -46,7 +47,7 @@ public class RampController : MonoBehaviour
         {
             if (Input.GetMouseButtonDown(0))
             {
-                throwPoint++;
+                throwPoint+=pointPerClick;
                 fillAmount = throwPoint / maxThrowPoint;
                 //TapBar.fillAmount = throwPoint / maxThrowPoint;
                 Debug.Log(throwPoint);
